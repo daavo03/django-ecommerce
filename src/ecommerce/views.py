15a74ad7f2ec37668712cd72f24062b99bc36283
Daniel Vazquez <daavo.1002@gmail.com>
2023-11-07 00:21:24 -0600

    Django forms

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .forms import ContactForm
+
 
 def home_page(request):
   context = {
@@ -17,13 +19,12 @@ def about_page(request):
   return render(request, "home_page.html", context)
 
 def contact_page(request):
+  contact_form = ContactForm(request.POST or None)
   context = {
     "title": "Contact",
-    "content": "Welcome to the contact page."
+    "content": "Welcome to the contact page.",
+    "form": contact_form
   }
-  if request.method == "POST":
-    print(request.POST)
-    print(request.POST.get("fullname"))
-    print(request.POST.get("email"))
-    print(request.POST.get("content"))
+  if contact_form.is_valid():
+    print(contact_form.cleaned_data)
   return render(request, "contact/view.html", context)
