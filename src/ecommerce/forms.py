c7f12439f7560ed91e7cf7583d63d55c9d8754aa
Daniel Vazquez <daavo.1002@gmail.com>
2023-11-07 01:09:04 -0600

    User register

--- src/ecommerce/forms.py
+++ src/ecommerce/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class ContactForm(forms.Form):
   fullname = forms.CharField(
@@ -41,3 +44,40 @@ class LoginForm(forms.Form):
   password = forms.CharField(
     widget=forms.PasswordInput()
   )
+
+class RegisterForm(forms.Form):
+  username = forms.CharField()
+  email = forms.EmailField()
+  password = forms.CharField(
+    widget=forms.PasswordInput()
+  )
+  password2 = forms.CharField(
+    label="Confirm password",
+    widget=forms.PasswordInput()
+  )
+
+  def clean_username(self):
+    username = self.cleaned_data.get("username")
+    queryset = User.objects.filter(username=username)
+    if queryset.exists():
+      raise forms.ValidationError("Username is taken")
+
+    return username
+  
+  def clean_email(self):
+    email = self.cleaned_data.get("email")
+    queryset = User.objects.filter(email=email)
+    if queryset.exists():
+      raise forms.ValidationError("Email is taken")
+
+    return email
+
+  def clean(self):
+    data = self.cleaned_data
+    password = self.cleaned_data.get("password")
+    password2 = self.cleaned_data.get("password2")
+
+    if password2 != password:
+      raise forms.ValidationError("Passwords musth match.")
+    
+    return data
--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from .views import home_page, about_page, contact_page, login_page
+from .views import home_page, about_page, contact_page, login_page, register_page
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -24,4 +24,5 @@ urlpatterns = [
     url(r'^about/$', about_page),
     url(r'^contact/$', contact_page),
     url(r'^login/$', login_page),
+    url(r'^register/$', register_page),
 ]
